e57c327e11cf319e28c75c0ba1f1f9ba92e08e67
Sam Rossilli <sarossilli@gmail.com>
2023-12-29 23:53:02 +0000

    Feat: Setup react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <Home />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
