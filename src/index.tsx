fccbd7474b5604d027beaca9914145f0936e1239
Sam Rossilli <sarossilli@gmail.com>
2024-01-05 23:52:17 +0000

    fix: remove unused inports

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,6 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Navbar from './components/Navbar/Navbar';
 import PostDetails from './pages/PostDetails';
-import { Amplify } from 'aws-amplify';
-import { generateClient } from 'aws-amplify/api';
-import config from './amplifyconfiguration.json';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 //Amplify.configure(config);
