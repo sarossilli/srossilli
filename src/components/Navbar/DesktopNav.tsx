7f48a6c10e7df577b968e0ecff02ebfdb451bd11
Sam <sarossilli@gmail.com>
2024-01-28 16:15:10 +0000

    Fix: post styling and links

--- src/components/Navbar/DesktopNav.tsx
+++ src/components/Navbar/DesktopNav.tsx
@@ -6,7 +6,7 @@ function DesktopNav() {
     return (
         <>
             <div className={`flex justify-end w-full h-0 md:h-auto md:w-auto invisible md:visible`} id="navbar-default">
-                <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+                <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                     <li>
                         <NavLink to="/"
                             className={activeState} >
--- src/components/Navbar/util.ts
+++ src/components/Navbar/util.ts
@@ -1,5 +1,5 @@
-const activeClassName = "block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-const inactiveClassName = "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+const activeClassName = "block py-2 px-3 text-white md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+const inactiveClassName = "block py-2 px-3 text-gray-900 rounded md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:text-white"
 
 export const activeState = ({ isActive }: {
     isActive: any;
