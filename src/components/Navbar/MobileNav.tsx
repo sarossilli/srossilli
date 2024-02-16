1bec7e74ced5230f6b531fbf64059e4a6fb19bcd
Sam <sarossilli@gmail.com>
2024-02-16 13:10:28 +0000

    fix mobile navbar Z index

--- src/components/Navbar/MobileNav.tsx
+++ src/components/Navbar/MobileNav.tsx
@@ -5,9 +5,8 @@ import { activeState } from './util';
 function MobileNav({navbarOpen}: {navbarOpen: boolean}) {
     return (
         <>
-            <div className={`absolute justify-end w-full px-1 visible md:invisible ${(navbarOpen ? " flex" : " hidden")}`} id="navbar-default">
+            <div className={`absolute justify-end w-full px-1 visible md:invisible ${(navbarOpen ? " flex" : " hidden")} z-50`} id="navbar-default">
                 <ul className="font-medium flex bg-slate-900 flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:border-gray-700">
-
                     <li>
                         <NavLink to="/"
                             className={activeState} >
